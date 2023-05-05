906afbbcfeaa0ec045e18a2cca6aa6b8c0e676ea
Fotis Patseas <fpatseas@users.noreply.github.com>
2023-05-05 08:56:37 +0300

    style changes

--- src/background.js
+++ src/background.js
@@ -1,3 +1,5 @@
 chrome.runtime.onInstalled.addListener(() => {
+    // reset apiKey and chatHistory if extension is installed or updated, or when the browser is updated
     chrome.storage.sync.set({ apiKey: '' });
+    chrome.storage.sync.set({ chatHistory: '' });
 });
